f3542ff53085b4f1c221d8d10053f58545a6d4c8
J-a-y-a <144910001+J-a-y-a@users.noreply.github.com>
2023-11-11 13:22:22 +0530

    Add project files.

---
+++ AccordionExample/Controllers/HomeController.cs
@@ -0,0 +1,60 @@
+﻿using AccordionExample.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AccordionExample.Controllers
+{
+
+    public class HomeController : Controller
+    {
+        //jayaEntities db = new jayaEntities();
+        public ActionResult Index()
+        {
+
+            return View();
+        }
+
+        public ActionResult EmployeeForm()
+        {
+            return View();
+        }
+
+        // Action to display the Salary form
+        public ActionResult SalaryForm()
+        {
+            return View();
+        }
+
+        // Action to handle form submissions
+        [HttpPost]
+        public ActionResult SubmitForms(Emp employee, Salary salary)
+        {
+            if (ModelState.IsValid)
+            {
+                // Save data to the backend here (e.g., using Entity Framework)
+
+                // Assuming you have a DbContext named YourDbContext
+                using (jayaEntities  db = new jayaEntities())
+                {
+                    // Add employee and salary to the context
+                    db.Emps.Add(employee);
+                    db.Salaries.Add(salary);
+
+                    // Save changes to the database
+                    db.SaveChanges();
+                }
+
+                ViewBag.Message = "Data saved successfully!";
+            }
+            else
+            {
+                ViewBag.Message = "Form validation failed. Please check your inputs.";
+            }
+
+            return View("EmployeeSalaryForms");
+        }
+    }
+}
---
+++ AccordionExample/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AccordionExample.MvcApplication" Language="C#" %>
